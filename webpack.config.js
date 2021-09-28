6221f56c77460794cfd304b6b2c8c953d77a471a
Oleg Polyakov <olegpolyakov@outlook.com>
2021-09-28 14:29:19 +0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    name: 'lib',
+
+    mode: 'production',
+
+    entry: './scripts/material-components-web.js',
+
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'index.js',
+        library: {
+            type: 'window',
+            name: 'mdc'
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    CssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: [path.resolve('node_modules')]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new CssExtractPlugin({
+            filename: 'index.css'
+        })
+    ],
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false
+            }),
+            new CssMinimizerPlugin()
+        ]
+    }
+};
